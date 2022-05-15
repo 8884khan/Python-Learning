678b89712114efe387d0ea285b9118233c7f5910
8884khan <80153118+8884khan@users.noreply.github.com>
2022-05-15 03:51:12 -0700

    Update OOPS more.py

--- OOPS more.py
+++ OOPS more.py
@@ -26,3 +26,30 @@ print(Demo.__dict__)
 
 
 # Error in line 21 please help 
+
+class Demo:
+    a  =10
+    def __init__(self):
+        Demo.b = 20
+        print(Demo.b)
+    def fun1(self):
+        Demo.c=30
+        print(Demo.c)
+    @classmethod
+    def fun2(cls):
+        Demo.d = 40
+        cls.e =50
+        print(Demo.d)
+        print(cls.e)
+    @staticmethod
+    def fun3():
+        Demo.f = 60
+        print(Demo.f)
+print(Demo.a)
+ob = Demo
+ob.fun1(30)
+ob.fun2()
+ob.fun3()
+print(Demo.__dict__)
+
+# change argument and got answer but when run i am not getting the value 20
