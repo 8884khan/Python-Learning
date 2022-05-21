33f7a2c2ca0bba7075d7352c8f8c9042ae08711d
8884khan <80153118+8884khan@users.noreply.github.com>
2022-05-21 09:17:24 -0700

    Update polymorphism.py

--- polymorphism.py
+++ polymorphism.py
@@ -17,3 +17,15 @@ def fun(ob):
     ob.talk()
 cat = Duck()
 fun(cat)
+
+
+print("++++++++++++++++++++++++")
+#concatination 2 referance variable using magic methods
+class Book:
+    def __init__(self,pages):
+        self.pages=pages
+    def __add__(self,other):
+        return self.pages + other.pages
+ob1 = Book(100)
+ob2 = Book(50)
+print(ob1+ob2)
