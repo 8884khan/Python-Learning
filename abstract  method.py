0d728f8acf21655b511b18476609e544ef950bf4
8884khan <80153118+8884khan@users.noreply.github.com>
2022-06-02 07:30:47 -0700

    Update abstract  method.py

--- abstract  method.py
+++ abstract  method.py
@@ -10,4 +10,25 @@ class child(Demo):
         print("hello world")
 ob = child()
 ob.m()
+   
     
+    
+print("----------------------------------------------------")
+
+print("EXAMPLE 2")
+
+from abc import *
+class Demo(ABC):
+    @abstractmethod
+    def m1(self):
+        pass
+    
+    def m2(self):
+        print("non abstract method")
+class child(Demo):
+    def m1(self):
+        print("abstract method")
+c =child()
+c.m1()
+c.m2()
+
